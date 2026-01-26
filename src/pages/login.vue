38975e7d6555cdb97a09eafcd7bad862be6b134b
ChromeDev1 <armani.brown@moa.gov.jm>
2026-01-26 16:00:28 -0500

    changes added

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <section class="flex justify-center items-center w-full min-h-svh p-1">
+    <section class="flex justify-center items-center w-full min-h-svh">
        
     </section>
 </template>
